823c97f91082d39d9174b29059b6939bbc3bb107
MuammerBeytekin <mubyt_2005@hotmail.com>
2024-11-16 17:30:56 +0300

    Add files via upload

---
+++ DGD203/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace DGD203
+{
+    
+    public class Zoominator
+    {
+        public int Horsepower { get; private set; } 
+        public string JuiceType { get; private set; } 
+        public bool IsPurring { get; private set; } 
+
+        public Zoominator(int horsepower, string juiceType)
+        {
+            Horsepower = horsepower;
+            JuiceType = juiceType;
+            IsPurring = false; 
+        }
+
+        public void Ignite()
+        {
+            IsPurring = true;
+            Console.WriteLine("The Zoominator is purring now. Ready to go!");
+        }
+
+        public void Chill()
+        {
+            IsPurring = false;
+            Console.WriteLine("The Zoominator has been shut down. Time to relax.");
+        }
+    }
+
+    
+    public class JuiceCan
+    {
+        public int MaxJuice { get; private set; } 
+        public int CurrentJuice { get; private set; } 
+
+        public JuiceCan(int maxJuice)
+        {
+            MaxJuice = maxJuice;
+            CurrentJuice = maxJuice; 
+        }
+
+        public void SlurpJuice(int amount)
+        {
+            if (amount > 0)
+            {
+                CurrentJuice = Math.Max(0, CurrentJuice - amount); 
+            }
+        }
+
+        public void Rejuice(int amount)
+        {
+            if (amount > 0)
+            {
+                CurrentJuice = Math.Min(MaxJuice, CurrentJuice + amount); 
+            }
+        }
+    }
+
+    
+    public class Car
+    {
+        public string Color { get; set; }
+        public string LicenseNo { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public int ZoomLevel { get; private set; } 
+        public Zoominator Engine { get; private set; }
+        public JuiceCan FuelTank { get; private set; }
+
+        public Car(string color, string licenseNo, string make, int year, Zoominator engine, JuiceCan fuelTank)
+        {
+            Color = color;
+            LicenseNo = licenseNo;
+            Make = make;
+            Year = year;
+            Engine = engine;
+            FuelTank = fuelTank;
+            ZoomLevel = 0; 
+        }
+
+        public void Zoom()
+        {
+            if (!Engine.IsPurring)
+            {
+                Console.WriteLine("You can't zoom while the Zoominator is snoozing!");
+                return;
+            }
+
+            if (FuelTank.CurrentJuice > 0)
+            {
+                ZoomLevel += 10; 
+                FuelTank.SlurpJuice(5); 
+                Console.WriteLine($"Zooming! Speed: {ZoomLevel} km/h, Juice left: {FuelTank.CurrentJuice} L");
+            }
+            else
+            {
+                Console.WriteLine("Out of juice! Time to refill your JuiceCan.");
+            }
+        }
+
+        public void Slow()
+        {
+            if (ZoomLevel > 0)
+            {
+                ZoomLevel -= 10;
+                Console.WriteLine($"Slowing down... Speed: {ZoomLevel} km/h");
+            }
+            else
+            {
+                Console.WriteLine("Your ride is already at a standstill.");
+            }
+        }
+
+        public void Halt()
+        {
+            ZoomLevel = 0;
+            Console.WriteLine("The car has come to a complete stop. No more zoom.");
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {Make}\nYear: {Year}\nLicense No: {LicenseNo}\nColor: {Color}\nJuice Capacity: {FuelTank.MaxJuice} L\nCurrent Juice: {FuelTank.CurrentJuice} L\nZoom Level: {ZoomLevel} km/h\nEngine: {Engine.Horsepower} HP ({Engine.JuiceType})";
+        }
+    }
+}
---
+++ DGD203/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using DGD203;
+
+namespace ProgramConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan araba bilgilerini al
+            Console.Write("Enter the car's color: ");
+            string color = Console.ReadLine();
+
+            Console.Write("Enter the license number: ");
+            string licenseNo = Console.ReadLine();
+
+            Console.Write("Enter the manufacturer: ");
+            string make = Console.ReadLine();
+
+            Console.Write("Enter the year of production: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the fuel capacity (L): ");
+            int fuelCapacity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the engine horsepower: ");
+            int horsepower = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the engine juice type (e.g., Petrol, Diesel, Electric): ");
+            string engineType = Console.ReadLine();
+
+            // Zoominator ve JuiceCan nesnelerini oluştur
+            Zoominator engine = new Zoominator(horsepower, engineType);
+            JuiceCan fuelTank = new JuiceCan(fuelCapacity);
+
+            // Araba nesnesini oluştur
+            Car myCar = new Car(color, licenseNo, make, year, engine, fuelTank);
+
+            Console.WriteLine("\nYour ride is ready. Check it out:");
+            Console.WriteLine(myCar);
+
+            string choice;
+            do
+            {
+                Console.WriteLine("\n--- What's your next move? ---");
+                Console.WriteLine("1 - Start the Zoominator");
+                Console.WriteLine("2 - Stop the Zoominator");
+                Console.WriteLine("3 - Zoom Away");
+                Console.WriteLine("4 - Slow Down");
+                Console.WriteLine("5 - Refill the JuiceCan");
+                Console.WriteLine("6 - Show Car Details");
+                Console.WriteLine("0 - Exit");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        myCar.Engine.Ignite();
+                        break;
+                    case "2":
+                        myCar.Engine.Chill();
+                        myCar.Halt(); // Motor durunca hız sıfırlanıyor
+                        break;
+                    case "3":
+                        myCar.Zoom();
+                        break;
+                    case "4":
+                        myCar.Slow();
+                        break;
+                    case "5":
+                        Console.Write("How much juice to refill (L)? ");
+                        int juiceAmount = Convert.ToInt32(Console.ReadLine());
+                        myCar.FuelTank.Rejuice(juiceAmount);
+                        Console.WriteLine($"JuiceCan refilled! Current juice: {myCar.FuelTank.CurrentJuice} L");
+                        break;
+                    case "6":
+                        Console.WriteLine(myCar);
+                        break;
+                    case "0":
+                        Console.WriteLine("Goodbye! Drive safe.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            } while (choice != "0");
+        }
+    }
+}
